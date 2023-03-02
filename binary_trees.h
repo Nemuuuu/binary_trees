dd1c961a24af339ab7c1ce1441b15aa0d2a00e93
Neima Nesru <neimanesru34@gmail.com>
2023-03-01 23:01:13 -0800

    header file update

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES.H
-#define BINARY_TREES.H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,6 +29,7 @@ typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
